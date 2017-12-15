9e5db690380491f74fdb5b443a75ae480e963207
Trabalho <leonardocatarino@gmail.com>
2017-12-15 17:47:56 -0200

    dia 15/12

--- App2/src/app/app.module.ts
+++ App2/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID  } from '@angular/core';
 import { HttpModule } from '@angular/http'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ { provide: LOCALE_ID, useValue: 'pt-Br' } ],
--- App2/src/app/ordem-compra/ordem-compra.component.ts
+++ App2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { viewClassName } from '@angular/compiler';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -7,30 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public endereco: string = ''
-  public numero: string = ''
-  public complemento: string = ''
-  public formaPagamento: string = ''
+  @ViewChild('formulario') public formulario: NgForm
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public atualizaEndereco(endereco: string):void {
-    this.endereco = endereco
-  }
+  public confirmarCompra():void{
 
-  public atualizaNumero(numero: string):void {
-    this.numero = numero
   }
-
-  public atualizaComplemento(complemento: string):void {
-    this.complemento = complemento
-  }
-
-  public atualizaFormaPagamento(formaPagamento: string):void {
-    this.formaPagamento = formaPagamento
-  }
-
 }
